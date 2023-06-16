96a839ab367011e7184fc4ea670d6f8d9b690f31
username_anjastabuni <reblytabuni2004@gmail.com>
2023-06-16 18:20:59 +0700

    update materi php-batch-iii-day01

---
+++ tutorial/index.php
@@ -0,0 +1,27 @@
+<?php
+
+//komentar php menggunakan garis miring
+
+#komentar php menggunakan simbol pagar
+
+/*
+komentar php menggunakan garis miring dan simbol bintang.
+komentar jenis ini digunakan untuk komentar dengan banyak baris. 
+*/
+
+echo "<h1>Tutorial PHP</h1>";
+
+echo "
+<ul>
+    <li>
+        <a href='?page=home'>Home</a>
+    </li>
+    <li>
+        <a href='?page=intro'>Intro</a>
+    </li>
+    <li>
+        <a href='?page=instal'>Install</a>
+    </li>
+
+</ul>
+";
